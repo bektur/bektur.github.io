cb443111453196c33d9811f6b9088b6fcfb211a2
Bektur <tgbeko@gmail.com>
2022-03-14 20:55:49 +0600

    pushing new personal project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import DisplayPage from './components/DisplayPage';
+import Details from './components/Details';
+import data from './data.json';
+import { Card } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { createBrowserHistory } from "history";
+
+
+function App() {
+  const [exchanges, setExchange] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/exchanges')
+    .then(response => response.json())
+    .then((data) => {
+      setExchange(data.slice(0, 10));
+    })
+  }, []);
+    
+  return (
+    <>
+      <Router history={createBrowserHistory()}>    
+        <Routes>
+          <Route exact path="/" element={<DisplayPage data={exchanges}/>} />   
+
+          <Route exact path="/details/:id" element={<Details />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/DisplayPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+import { Card } from "react-bootstrap";
+
+function DisplayPage(props) {    
+    const { data } = props;  
+    
+    return (    
+        <Card className="text-center">            
+            <Card.Header>Top 10 Crtypto Exchanges</Card.Header>
+            <Card.Body>
+                <Table responsive striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th className="center">Rank</th>
+                            <th className="align-left">Name</th>
+                            <th className="align-left">Country</th>
+                            <th className="align-left">URL</th>                   
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { Object.keys(data).map((index) =>                             
+                            <tr key={data[index].id}>
+                                <td className="center">{data[index].trust_score_rank}</td>
+                                <td className="align-left"><img style={{width: "30px"}} src={data[index].image} />
+                                    <span style={{marginLeft: "5px"}}>                                
+                                        <Link to={`/details/${data[index].id}`}>{data[index].name}</Link>                                    
+                                    </span>
+                                </td>
+                                <td className="align-left">{data[index].country}</td>
+                                <td className="align-left"><a href={data[index].url}>{data[index].url}</a></td>                      
+                            </tr>
+                        )}                
+                    </tbody>
+                </Table>
+            </Card.Body>    
+        </Card>            
+    );
+}
+
+export default DisplayPage;
